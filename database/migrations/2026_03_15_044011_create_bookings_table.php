56dbd7b40c76b8e01a6c48ba927d565cc74f75ae
waiiiiiiiiiiiiiiiyaaaaaaa <mdawaikiandra2021@gmail.com>
2026-03-16 08:35:31 +0700

    Update 2026_03_15_044011_create_bookings_table.php

--- database/migrations/2026_03_15_044011_create_bookings_table.php
+++ database/migrations/2026_03_15_044011_create_bookings_table.php
@@ -8,24 +8,17 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('bookings', function (Blueprint $table) {
-
+        Schema::create('meja', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_pelanggan');
-            $table->string('no_hp');
-            $table->date('tanggal_booking');
-            $table->time('jam_booking');
-            $table->integer('jumlah_orang');
-            $table->integer('nomor_meja');
-            $table->text('menu');
-            $table->text('catatan')->nullable();
+            $table->integer('no_meja');
+            $table->integer('kapasitas');
+            $table->string('status')->default('tersedia');
             $table->timestamps();
-
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('meja');
     }
 };
