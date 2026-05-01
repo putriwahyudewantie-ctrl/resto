5a63506f943d22f181dd0027feeb20214df42930
farelrizkipratama <farelrizkypratama3@gmail.com>
2026-05-01 21:37:33 +0700

    Perbaikan Pembayaran DP

--- resources/views/booking/show.blade.php
+++ resources/views/booking/show.blade.php
@@ -154,7 +154,7 @@
                             <!-- Path disesuaikan dengan folder baru Anda -->
                             <img src="{{ asset('images/menu/qrdewantie.png') }}" 
                                 alt="DANA Dewantie" 
-                                style="width: 230px; border-radius: 12px; box-shadow: 0 4px 15px rgba(0,0,0,0.1); border: 5px solid #fff;">
+                                style="width: 225px; border-radius: 12px; box-shadow: 0 4px 15px rgba(0,0,0,0.1); border: 5px solid #fff;">
                         </div>
 
                         <div class="payment-details mb-3">
