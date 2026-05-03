ac958eae927c93ca1e7be4103e87291128512ee6
farelrizkipratama <farelrizkypratama3@gmail.com>
2026-05-03 22:21:01 +0700

    B

---
+++ database/migrations/2026_05_03_141435_create_bookings_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('bookings', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained();
+        $table->string('nama_pelanggan');
+        $table->decimal('total_harga', 12, 2);
+        $table->decimal('dp', 12, 2);
+        
+        // 4 Status yang Anda inginkan
+        $table->enum('status', ['pending_dp', 'pending', 'selesai', 'dibatalkan'])->default('pending_dp');
+        
+        $table->timestamps();
+    });
+}
+};
